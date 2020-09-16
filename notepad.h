dba79b53b1ef7d5b208e3eaca7109168d9b4df8c
letol <tolomei.leonardo@gmail.com>
2020-09-16 18:26:39 +0200

    from TextEdit to Notepad

--- notepad.h
+++ notepad.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2017 The Qt Company Ltd.
+** Copyright (C) 2018 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the documentation of the Qt Toolkit.
@@ -48,14 +48,75 @@
 **
 ****************************************************************************/
 
-#include "notepad.h"
-#include <QApplication>
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
 
-int main(int argc, char *argv[])
-{
-    QApplication EditorApp(argc, argv);
-    Notepad Editor;
-    Editor.show();
+//! [all]
+//! [1]
+#include <QMainWindow>
+//! [1]
 
-    return EditorApp.exec();
+//! [2]
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Notepad;
 }
+QT_END_NAMESPACE
+//! [2]
+
+//! [3]
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+//! [3]
+
+//! [4]
+public:
+    explicit Notepad(QWidget *parent = nullptr);
+//! [4]
+//! [5]
+    ~Notepad();
+//! [5]
+
+private slots:
+    void newDocument();
+
+    void open();
+
+    void save();
+
+    void saveAs();
+
+    void print();
+
+    void exit();
+
+    void copy();
+
+    void cut();
+
+    void paste();
+
+    void undo();
+
+    void redo();
+
+    void selectFont();
+
+    void setFontBold(bool bold);
+
+    void setFontUnderline(bool underline);
+
+    void setFontItalic(bool italic);
+
+    void about();
+
+//! [6]
+private:
+    Ui::Notepad *ui;
+    QString currentFile;
+//! [6]
+};
+//! [all]
+
+#endif // NOTEPAD_H
